550f6d1f119b8cf15de4317a4dd47590da9c23f1
radekxrandom <jozwa.zawadiaka@gmail.com>
2020-11-16 11:41:20 +0100

    cleanr code

--- contentScript.js
+++ contentScript.js
@@ -1,31 +1,25 @@
-var clickedEl = null;
-var x;
-var y;
-var bubbleDOM = document.createElement("div");
+let x;
+let y;
+let bubbleDOM = document.createElement("div");
 bubbleDOM.setAttribute("class", "selection_bubble");
 document.body.appendChild(bubbleDOM);
 
 document.addEventListener(
   "contextmenu",
-  function(event) {
-    /*  clickedEl = event.target;
-    y = event.pageY - 25;
-    x = event.pageX; */
-    var r = window
+  event => {
+    let r = window
       .getSelection()
       .getRangeAt(0)
       .getBoundingClientRect();
-    y = r.top - 120;
+    y = r.top - 150;
     x = (r.right + r.left) / 2;
-    console.log(r);
   },
   true
 );
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.name == "getClickedEl") {
-    console.log(clickedEl);
-    renderBubble(x, y, request.msg);
+chrome.runtime.onMessage.addListener((request, sender) => {
+  if (request.name === "showTooltip") {
+    renderBubble(x, y, request.translated);
   }
 });
 
@@ -37,12 +31,11 @@ const createTranslatedHtml = wordsArr => {
   return word;
 };
 
-function renderBubble(mouseX, mouseY, selection) {
-  console.log(selection.translations);
+const renderBubble = (x, y, selection) => {
   const trs = createTranslatedHtml(selection.translations);
 
-  bubbleDOM.style.top = mouseY + "px";
-  bubbleDOM.style.left = mouseX + "px";
+  bubbleDOM.style.top = y + "px";
+  bubbleDOM.style.left = x + "px";
   bubbleDOM.style.padding = "0.4rem";
   bubbleDOM.style.background = "gainsboro";
   bubbleDOM.style.visibility = "visible";
@@ -53,7 +46,7 @@ function renderBubble(mouseX, mouseY, selection) {
   </div>
   `;
   document.getElementById("closebtn").addEventListener("click", closeTooltip);
-}
+};
 
 const closeTooltip = e => {
   const tooltip = document.getElementsByClassName("selection_bubble")[0];
--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-const setUpContextMenus = () => {
-  /*  chrome.contextMenus.create({
-      type: "separator",
-      id: "sep1",
-      contexts: ["selection"]
-    }); */
-  chrome.contextMenus.create({
-    title: 'Translate "%s" to English',
-    id: "123123",
-    contexts: ["selection"],
-    onclick: (info, tab) => {
-      checkTranslation(info.selectionText);
-    }
-  });
-};
-
-chrome.runtime.onInstalled.addListener(function() {
+const setUpContextMenu = () => {
   chrome.contextMenus.create({
     title: 'Translate "%s" to English',
     id: "123123",
     contexts: ["selection"]
   });
-  //setUpContextMenus();
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+  setUpContextMenu();
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
@@ -43,7 +30,6 @@ const getRoot = async word => {
   console.log(root);
   return root;
 };
-/*   `https://cors-anywhere.herokuapp.com/https://www.online-latin-dictionary.com/latin-english-dictionary.php?parola=${word}`, */
 
 const getTranslation = async word => {
   const result = await fetch(
@@ -59,8 +45,6 @@ const getTranslation = async word => {
   const matches = resText.match(
     /(?<=\<span class=\"english\">).*(?=\<\/span>)/gi
   );
-  console.log(matches);
-  console.log(matches[0]);
   const content = {
     latinWord: word,
     translations: matches
@@ -69,12 +53,6 @@ const getTranslation = async word => {
 };
 
 const checkTranslation = async (selection, tab) => {
-  //alert(selection);
-  /*chrome.tabs.sendMessage(tab.id, "getClickedEl", function(cords) {
-    let { x, y } = cords;
-    console.log(x);
-    console.log(y);
-  });*/
   if (selection.split(" ").length > 1) {
     chrome.extension
       .getBackgroundPage()
@@ -82,13 +60,9 @@ const checkTranslation = async (selection, tab) => {
     console.log("I can translate only one word at the time");
     return;
   }
-  console.log(chrome.tabs.url);
-  chrome.extension.getBackgroundPage().console.log(selection);
-  chrome.tabs.executeScript({ code: "console.log('test');" }, () => {
-    console.log("test");
-  });
+
   const root = await getRoot(selection);
   const translated = await getTranslation(root);
   console.log(selection);
-  chrome.tabs.sendMessage(tab.id, { name: "getClickedEl", msg: translated });
+  chrome.tabs.sendMessage(tab.id, { name: "showTooltip", translated });
 };
