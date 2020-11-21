b79f8aff9ad50b31d59b6216af09615f5113616e
radekxrandom <jozwa.zawadiaka@gmail.com>
2020-11-21 17:34:48 +0100

    commit

--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,105 @@
 let x;
 let y;
-let bubbleDOM = document.createElement("div");
+const bubbleDOM = document.createElement("div");
 bubbleDOM.setAttribute("class", "selection_bubble");
+const xpkej = document.createElement("div");
 document.body.appendChild(bubbleDOM);
 
 document.addEventListener(
-  "contextmenu",
-  event => {
-    let r = window
-      .getSelection()
-      .getRangeAt(0)
-      .getBoundingClientRect();
-    y = r.top - 150;
-    x = (r.right + r.left) / 2;
-  },
-  true
+	"contextmenu",
+	event => {
+		const r = window.
+			getSelection().
+			getRangeAt(0).
+			getBoundingClientRect();
+		y = r.top + window.scrollY;
+		x = (r.right + r.left) / 2 + window.scrollX;
+		x -= r.width * 0.2;
+		console.log(r);
+	},
+	true
 );
 
-chrome.runtime.onMessage.addListener((request, sender) => {
-  if (request.name === "showTooltip") {
-    renderBubble(x, y, request.translated);
-  }
-});
+const closeTooltip = e => {
+	const tooltip = document.getElementsByClassName("selection_bubble")[0];
+	//tooltip.style.visibility = "hidden";
+	tooltip.classList.remove("shown");
+	bubbleDOM.style.marginTop = "unset";
+	tooltip.className = "selection_bubble";
+	console.log(tooltip);
+};
 
 const createTranslatedHtml = wordsArr => {
-  let word = "";
-  for (let i = 0; i < wordsArr.length; i++) {
-    word = word + `<p>${i}. ${wordsArr[i]}</p>` + "\n";
-  }
-  return word;
+	console.log(wordsArr);
+	let word = "";
+	for (let i = 0; i < wordsArr.length; i++) {
+		word += `<p>${i}. ${wordsArr[i]}</p> \n`;
+	}
+	return word;
+};
+
+const closeOnClickOutside = e => {
+	const tooltip = document.getElementById("transTip");
+	const closeBtn = document.getElementById("closebtn");
+	const checkTooltip = tooltip.contains(e.target);
+	const checkBtn = closeBtn.contains(e.target);
+	if (!checkTooltip || checkBtn) {
+		closeTooltip();
+	}
 };
 
 const renderBubble = (x, y, selection) => {
-  const trs = createTranslatedHtml(selection.translations);
-
-  bubbleDOM.style.top = y + "px";
-  bubbleDOM.style.left = x + "px";
-  bubbleDOM.style.padding = "0.4rem";
-  bubbleDOM.style.background = "gainsboro";
-  bubbleDOM.style.visibility = "visible";
-  bubbleDOM.innerHTML = `
-  <div class='tooltip'>
-    <div class='latin_word'>${selection.latinWord} <span id="closebtn">[x]</span></div>
+	const trs = createTranslatedHtml(selection.translations);
+	//bubbleDOM.style.padding = "0.4rem";
+	bubbleDOM.style.background = "#f1f1f1";
+	//bubbleDOM.style.visibility = "visible";
+	bubbleDOM.classList.add("shown");
+	bubbleDOM.innerHTML = `
+  <div class='translationTooltip' id='transTip'>
+    <div class='latin_word'>${selection.latinWord} <span id="closebtn">x</span></div>
     <div class='translations'>${trs}</div>
   </div>
   `;
-  document.getElementById("closebtn").addEventListener("click", closeTooltip);
+
+	const dimensions = bubbleDOM.getBoundingClientRect();
+
+	if (
+		dimensions.width + x >=
+    (window.innerWidth || document.documentElement.clientWidth)
+	) {
+		bubbleDOM.style.left = x - dimensions.width + "px";
+	} else {
+		bubbleDOM.style.left = x + "px";
+	}
+
+	if (y - dimensions.height - window.scrollY < 20) {
+		bubbleDOM.style.top = y + "px";
+		bubbleDOM.style.marginTop = "1em";
+		bubbleDOM.classList.add("arrow_top");
+	} else {
+		bubbleDOM.style.top = y - dimensions.height + "px";
+		bubbleDOM.style.marginTop = "-1em";
+		bubbleDOM.classList.add("arrow_bottom");
+	}
+
+	document.addEventListener("click", closeOnClickOutside);
 };
 
-const closeTooltip = e => {
-  const tooltip = document.getElementsByClassName("selection_bubble")[0];
-  tooltip.style.visibility = "hidden";
-  console.log(tooltip);
+const adjustModalForPdf = () => {
+	console.log("pach");
+	bubbleDOM.style.top = "30%";
+	bubbleDOM.style.left = "50%";
+	bubbleDOM.style.transform = "translate(-50%, 0%)";
+	bubbleDOM.style.position = "fixed";
+	bubbleDOM.style.zIndex = "10";
 };
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+	if (request.name === "showTooltip") {
+		const pdfRegex = /^.*\.(doc|DOC|pdf|PDF)$/;
+		const path = window.location.pathname;
+		const isPdf = pdfRegex.test(path);
+		renderBubble(x, y, request.translated);
+		isPdf && adjustModalForPdf();
+	}
+});
--- main.js
+++ main.js
@@ -1,68 +1,126 @@
 const setUpContextMenu = () => {
-  chrome.contextMenus.create({
-    title: 'Translate "%s" to English',
-    id: "123123",
-    contexts: ["selection"]
-  });
+	chrome.contextMenus.create({
+		title: "Translate \"%s\" to English",
+		id: "123123",
+		contexts: ["selection"]
+	});
 };
 
 chrome.runtime.onInstalled.addListener(() => {
-  setUpContextMenu();
+	setUpContextMenu();
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  checkTranslation(info.selectionText, tab);
+	checkTranslation(info.selectionText, tab);
 });
 
 const getRoot = async word => {
-  const result = await fetch(
-    `https://latinwordnet.exeter.ac.uk/lemmatize/${word}`,
-    {
-      headers: {
-        "x-requested-with": "XMLHttpRequest",
-        Origin: "*"
-      }
-    }
-  );
-  const resJson = await result.json();
-  console.log(resJson);
-  const root = resJson[0].lemma.lemma;
-  console.log(root);
-  return root;
+	const result = await fetch(
+		`https://latinwordnet.exeter.ac.uk/lemmatize/${word}`,
+		{
+			headers: {
+				"x-requested-with": "XMLHttpRequest",
+				Origin: "*"
+			}
+		}
+	);
+	const resJson = await result.json();
+	console.log(resJson);
+	const root = resJson[0].lemma.lemma;
+	console.log(root);
+	return root;
 };
 
 const getTranslation = async word => {
-  const result = await fetch(
-    `https://www.online-latin-dictionary.com/latin-english-dictionary.php?parola=${word}`,
-    {
-      headers: {
-        "x-requested-with": "XMLHttpRequest",
-        Origin: "*"
-      }
-    }
-  );
-  const resText = await result.text();
-  const matches = resText.match(
-    /(?<=\<span class=\"english\">).*(?=\<\/span>)/gi
-  );
-  const content = {
-    latinWord: word,
-    translations: matches
-  };
-  return content;
+	const result = await fetch(
+		`https://www.online-latin-dictionary.com/latin-english-dictionary.php?parola=${word}`,
+		{
+			headers: {
+				"x-requested-with": "XMLHttpRequest",
+				Origin: "*"
+			}
+		}
+	);
+	const resText = await result.text();
+	const matches = resText.match(/(?<=<span class="english">).*(?=<\/span>)/gi);
+	if (!matches) {
+		throw new Error();
+	}
+	const content = {
+		latinWord: word,
+		translations: matches
+	};
+	return content;
+};
+
+const altTranslation = async word => {
+	const result = await fetch(
+		`http://www.perseus.tufts.edu/hopper/morph?l=${word}&la=la`,
+		{
+			headers: {
+				"x-requested-with": "XMLHttpRequest",
+				Origin: "*"
+			}
+		}
+	);
+	const resText = await result.text();
+	const translation = resText.match(
+		/(?<=<span class="lemma_definition">)[\w,\n\t\s]*(?=(\s+)?<\/span>)/gi
+	);
+	const lemma = resText.match(/(?<=<h4 class=\"la\">).*(?=<\/h4>)/gi);
+	console.log(lemma);
+	console.log(translation);
+	let translatedArr;
+	let content;
+	if (lemma.length > 1) {
+		translatedArr = translation.map(el => el.trim());
+		const data = lemma.map((el, i) => el + " - " + translatedArr[i]);
+		content = {
+			latinWord: "Multiple words found",
+			translations: data
+		};
+	} else {
+		translatedArr = translation[0].split(",");
+		translatedArr[0] = translatedArr[0].trim();
+		content = {
+			latinWord: lemma[0],
+			translations: translatedArr
+		};
+	}
+	console.log(translatedArr);
+	console.log(content);
+	return content;
 };
 
 const checkTranslation = async (selection, tab) => {
-  if (selection.split(" ").length > 1) {
-    chrome.extension
-      .getBackgroundPage()
-      .console.log("I can translate only one word at the time");
-    console.log("I can translate only one word at the time");
-    return;
-  }
-
-  const root = await getRoot(selection);
-  const translated = await getTranslation(root);
-  console.log(selection);
-  chrome.tabs.sendMessage(tab.id, { name: "showTooltip", translated });
+	if (selection.split(" ").length > 1) {
+		chrome.extension.
+			getBackgroundPage().
+			console.log("I can translate only one word at the time");
+		console.log("I can translate only one word at the time");
+		return;
+	}
+	let translated;
+	const latinWord = selection.trim();
+	/*
+	 *	if (selection.match(/(?<=\w)que$/)) {
+	 *	latinWord = selection.match(/(\w+)que$/)[1];
+	 *	}
+	 */
+	try {
+		const root = await getRoot(latinWord);
+		translated = await getTranslation(root);
+	} catch (err) {
+		console.log(err);
+		translated = await altTranslation(latinWord);
+	}
+	console.log(selection);
+	console.log(latinWord);
+	console.table(translated);
+	console.dir(tab);
+	console.log(tab.id);
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+		const tabb = tabs[0];
+		chrome.tabs.sendMessage(tabb.id, {name: "showTooltip", translated});
+	});
 };
